aa2021117f06349c5b3439ded36c271648aaf1c5
Natarajan <nataraj.itsolution@gmail.com>
2023-03-22 14:43:21 +0530

    Middleware

--- src/Redux/Product/store.js
+++ src/Redux/Product/store.js
@@ -1,8 +1,9 @@
 //import {legacy_createStore} from 'redux'
 import {combineReducers, configureStore} from '@reduxjs/toolkit'
 import {batreducer,padreducer} from './ProductReducer'
+import logger from 'redux-logger'
 
 const rootreducer=combineReducers({bat:batreducer,pad:padreducer})
 
-const store=configureStore({reducer:rootreducer});
+const store=configureStore({reducer:rootreducer,middleware:[logger]});
 export default store;
